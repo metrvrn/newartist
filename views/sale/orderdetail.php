949d2a743ef56127dffd2d8dca0e98cd18e97267
RomanMk <roman.mokrinskij@gmail.com>
2018-07-25 12:45:24 +0300

    25 07 2018 12 45

--- views/sale/orderdetail.php
+++ views/sale/orderdetail.php
@@ -17,45 +17,31 @@ $this->title = 'данные заказа';
 		<?//=CatalogMenu::widget(['model' => $catalogModel])?>
   </div>
 	<div class="col-xs-12 col-md-9">
-
-	   <?echo $this->title;  ?>
-	   <br>
-	    
-	   <?echo $model->newOrderId;  ?>
-	   
-	   <br>
-	   <?echo $model->name;?>
-	   <br>
-	     <?echo $model->email;?>
-	   <br>
-	     <?echo $model->phone;?>
-	   <br>
-	      <?echo $model->adress;?>
-	   <br>   <?echo $model->comment;?>
-	   <br>
-	   <?echo "Model id: ". $model->orderId;  ?>
-	   
-	   <br>
-	   
-	   
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<span class="order-detail__order-number">
+					Заказ № <?=$model->orderId;?>
+				</span>
+			</div>
+			<div class="panel-body">
+				<div class="order-detail__order-address">
+					<?=$model->adress;?>
+				</div>
+				<?php if(isset($model->comment)) : ?>
+					<div class="order-detail__order-comment">
+						<span>Комментарий: <?=$model->comment;?></span>
+					</div>
+				<?php endif; ?>
+				<div class="order-detail__order-sum">
+					<?//=$model->summ;?>
+				</div>
+			</div>
+		</div>
 	  <?
 		foreach($model->basketArray as $item){
-			
-			
 			echo $item['name'].' id ='.$item['id'].'<br>';
 			echo $item['image'].' id ='.$item['id'].'<br>';
-			
-			
 		}
-			
-			
-			
-	  
 	  ?>
-	   
-	   
 	</div>
-    <div id="links" class="col-xs-12 col-md-9">
-       
-    </div>
 </div>
